88d8389d861bd4f6c36fd365803673a8f830be26
Aniket Bisure <bisureaniket03@gmail.com>
2026-01-08 18:13:12 +0530

    Make footer responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins, Akshar } from "next/font/google";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const hind = localFont({
-  src: [
-    {
-      path: "../../public/Hind-Light.ttf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../../public/Hind-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/Hind-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/Hind-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/Hind-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  variable: "--font-hind",
-});
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -59,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="mr" suppressHydrationWarning>
-      <body className={`${hind.variable} ${poppins.variable} ${akshar.variable}`}>{children}</body>
+      <body className={`${poppins.variable} ${akshar.variable}`}>{children}</body>
     </html>
   );
 
