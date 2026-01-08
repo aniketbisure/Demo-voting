f9d09261326de6ed6cec34fa4cbe533d5475af6d
Aniket Bisure <bisureaniket03@gmail.com>
2026-01-08 17:55:55 +0530

    Add and configure Hind local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,38 @@
 import type { Metadata } from "next";
 import { Poppins, Akshar } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const hind = localFont({
+  src: [
+    {
+      path: "../../public/Hind-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/Hind-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/Hind-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/Hind-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/Hind-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-hind",
+});
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -27,7 +59,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="mr" suppressHydrationWarning>
-      <body className={`${poppins.variable} ${akshar.variable}`}>{children}</body>
+      <body className={`${hind.variable} ${poppins.variable} ${akshar.variable}`}>{children}</body>
     </html>
   );
 
