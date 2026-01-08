6cfe7e7690964ccb14c688cad553ea9f9b7ef653
Aniket Bisure <bisureaniket03@gmail.com>
2026-01-08 17:47:41 +0530

    Remove candidate photo from symbol column

--- src/app/demo/[id]/DemoClient.tsx
+++ src/app/demo/[id]/DemoClient.tsx
@@ -177,22 +177,12 @@ export default function DemoClient({ poll }: { poll: Poll }) {
                                                 </td>
                                                 <td className={styles.cellSymbol}>
                                                     {isMainRow && (
-                                                        <div className={styles.symbolContainer}>
-                                                            <div className={styles.symbolFlex}>
-                                                                <img src={poll.mainSymbolUrl}
-                                                                    alt="Party Symbol"
-                                                                    className={styles.partySymbolImg} />
-                                                                {candidate.symbolUrl !== poll.mainSymbolUrl && (
-                                                                    <>
-                                                                        <div className={styles.symbolDivider}></div>
-                                                                        <img src={candidate.symbolUrl}
-                                                                            alt="Candidate Photo"
-                                                                            className={styles.candidatePhotoImg} />
-                                                                    </>
-                                                                )}
-                                                            </div>
+                                                        <>
+                                                            <img src={poll.mainSymbolUrl}
+                                                                alt="Symbol"
+                                                                className={styles.symbolImg} />
                                                             {isVoted && <div className={styles.redLamp}></div>}
-                                                        </div>
+                                                        </>
                                                     )}
                                                 </td>
                                                 <td className={styles.cellButton}>
